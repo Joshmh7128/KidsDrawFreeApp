b6cc4436a2ea94f2dd37d89b5da46d348d14e120
Josh <joshmh7128@gmail.com>
2020-10-06 10:50:50 -0400

    Added scene manager, URL link

---
+++ Assets/Scripts/LocalSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LocalSceneManager : MonoBehaviour
+{
+    /// this script lets us go from the main menu to any of our different scenes. it loads scenes singularly, not additively
+    /// Buttons are assigned functions from the editor
+
+    // Open New Scene
+    public void CustomLoadScene(string targetScene)
+    {
+        SceneManager.LoadScene(targetScene, LoadSceneMode.Single);
+    }
+
+    // Opens URL
+    public void OpenURL(string targetURL)
+    {
+        Application.OpenURL(targetURL);
+    }
+}
